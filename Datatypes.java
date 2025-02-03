55d7a0148c7071c194f53a228fc657f4b280dfc1
swathireddy369 <swathiamaravadi53@gmail.com>
2025-02-03 13:50:12 -0500

    A basic code regarding datatypes,loops,operators and typeconversionand casting

---
+++ Datatypes.java
@@ -0,0 +1,20 @@
+class Datatypes {
+    public static void main(String a[]) {
+
+        //literals
+        int num=0b101;
+        System.out.println(num);
+        //
+        byte by = 127; // 1byte
+        short sh = 558; // 2bytes
+        int num1 = 9; // 4bytes
+        long l = 5854l; // 8bytes
+
+        float f = 5.8f; // 4bytes
+        double d = 5.8;// 8bytes
+
+        char c = 'k';// 2bytes
+        boolean b = true; // 1byte
+
+    }
+}
---
+++ TypeConersationAndCasting.java
@@ -0,0 +1,16 @@
+public class TypeConersationAndCasting {
+   public static void main(String args[]){
+    int a=257;
+    byte b=(byte)a;
+    float f = 5.6f;
+    int t=(int)f;
+    System.out.println(b);
+    System.out.println(t);
+    byte c=10;
+    byte d=30;
+    int s=c*d;
+    System.out.println(s);
+
+
+   } 
+}
